7bbe0fbea9e218750e72381428ec8bb29c93a2ec
sayadlou <s.sayadlou@gmail.com>
2021-12-01 19:46:51 +0330

    config for heroku

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['arad-imeg.herokuapp.com','127.0.0.1']
 
 MIDDLEWARE += [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
