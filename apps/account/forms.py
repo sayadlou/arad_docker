d91e799fd8a34915451299ccb3bfe3113a1c0b1b
sayadlou <s.sayadlou@gmail.com>
2021-12-03 16:41:32 +0330

    translation to persian language added

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import AuthenticationForm, UsernameField, PasswordResetForm, PasswordChangeForm, \
     SetPasswordForm
 from django.utils.translation import gettext_lazy as _
@@ -27,8 +28,36 @@ class MyPasswordResetForm(PasswordResetForm):
 
 
 class MyPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label=_("Old password"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={'class': "form-control", 'autocomplete': 'current-password', 'autofocus': True}),
+    )
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={'class': "form-control", 'autocomplete': 'new-password'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': "form-control", 'autocomplete': 'new-password'}),
+    )
     captcha = CaptchaField(widget=CustomCaptchaTextInput(attrs={'class': "form-control"}))
 
 
 class MySetPasswordForm(SetPasswordForm):
     captcha = CaptchaField(widget=CustomCaptchaTextInput(attrs={'class': "form-control"}))
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={'class': "form-control", 'autocomplete': 'new-password'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': "form-control", 'autocomplete': 'new-password'}),
+    )
