2bf8f42d8a0fe4fca61b6977f689ccd44a9deb2d
sayadlou <s.sayadlou@gmail.com>
2022-03-17 20:14:56 +0330

    azbankgateways debug 3

--- django_backend/entrypoint.sh
+++ django_backend/entrypoint.sh
@@ -12,8 +12,8 @@ then
 fi
 
 #python manage.py flush --no-input
+python manage.py makemigrations azbankgateways
 python manage.py collectstatic  --noinput
-python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
