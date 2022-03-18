7838bf4b4b3a049ad1d3519700dbe9a2072f954e
sayadlou <s.sayadlou@gmail.com>
2022-03-18 10:28:47 +0330

    azbankgateways migration  debug 3

--- django_backend/apps/store/migrations/0002_payment_transaction.py
+++ django_backend/apps/store/migrations/0002_payment_transaction.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('azbankgateways', '__last__'),
+        ('azbankgateways', '__latest__'),
         ('store', '0001_initial'),
     ]
 
